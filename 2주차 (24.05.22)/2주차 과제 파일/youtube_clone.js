39dc019ab7b5e69ab4f44db755bebafe5228fee7
manNomi <hanmw110@naver.com>
2024-05-29 15:35:14 +0900

    fix header & add function

--- 2주차 (24.05.22)/2주차 과제 파일/youtube_clone.js
+++ 2주차 (24.05.22)/2주차 과제 파일/youtube_clone.js
@@ -1,12 +1,15 @@
-// 변수
+// 전역변수
 var search_btn_text =document.getElementById('search_btn_text')
 var search_form =document.getElementById('search_form')
 var search_shape =document.getElementById('search_shape')
 var search_event_btn =document.getElementById('search_event_btn')
 var space =document.getElementById('space')
 var youtube_logo = document.getElementById('youtube_logo')
+var search_btn=document.getElementById("search_btn")
+var main=document.getElementById("main")
 var focus_obj=true
 var length_width=0
+
 
 
 // 검색창 줄어들면 삭제하기
@@ -14,9 +17,11 @@ const observer = new ResizeObserver(Obs => {
   for (let Ob of Obs) {
     const length = Ob.contentRect
     var length_width=length.width
-    if (length_width<113){
+    if (length_width<123){
         search_shape.style="display:none;"
-      }}})
+      }}
+    }
+    )
 
 
 // 테스트코드
@@ -51,4 +56,92 @@ search_btn_text.onblur=function(){
     space.style="width:70px;"
     focus_obj=true
   }
+}
+
+
+// 유튜브 검색기능 구현
+search_btn.onclick=function(){
+  // var text = document.getElementById("search_btn_text").value
+  document.location.href="https://www.youtube.com/results?search_query="+search_btn_text.value;
+}
+// 아스키 키코드 13 = enter 임을 이용
+search_btn_text.onkeydown=function(input){
+  if(input.keyCode==13){
+  document.location.href="https://www.youtube.com/results?search_query="+search_btn_text.value;
+  }
+}
+
+
+window.onload=function(){
+
+  // 썸네일 개수
+  var thumb_index=15
+  var thumb_img= []
+  var thumb_userImg =[]
+  var thumb_title=[]
+  var tumb_title_user=[]
+  var tumb_view=[]
+  var tumb_date=[]
+
+  for(i=0;i<thumb_index;i++){
+    thumb_img.push("image/png/main.play_img.PNG")
+    thumb_userImg.push("image/png/main.playname_img.PNG")
+    thumb_title.push("𝐏𝐥𝐚𝐲𝐥𝐢𝐬𝐭 돌아가고 싶은 그때 그 시절 2010년 감성힙합 I 다이나믹듀오, 프라이머리, 긱스, 개리, 빈지노")
+    tumb_title_user.push("올끌 (All of MBClassic)")
+    tumb_view.push("조회수"+44+"만회 • ")
+    tumb_date.push(8+"개월 전")
+  }
+
+
+  // 메인 화면생성
+  for(i=0;i<thumb_index;i++){
+
+  // 썸네일 큰테두리
+  var new_mainGroupShape=document.createElement("div");
+  new_mainGroupShape.setAttribute("class","main_group_shape")
+  main.appendChild(new_mainGroupShape)
+
+  // 썸네일
+  var new_mainImg=document.createElement("img");
+  new_mainImg.setAttribute("class","main_img")
+  new_mainImg.setAttribute("src",thumb_img[i])
+  new_mainGroupShape.appendChild(new_mainImg)
+
+  var new_mainThumbAll=document.createElement("div");
+  new_mainThumbAll.setAttribute("class","container_center")
+  new_mainGroupShape.appendChild(new_mainThumbAll)
+
+  var new_mainThumb_user=document.createElement("img");
+  new_mainThumb_user.setAttribute("src",thumb_userImg[i])
+  new_mainThumbAll.appendChild(new_mainThumb_user)
+
+  var new_mainThumb=document.createElement("div");
+  new_mainThumbAll.appendChild(new_mainThumb)
+
+  var new_mainThumb_title=document.createElement("p");
+  new_mainThumb_title.setAttribute("class","main_text_line")
+  new_mainThumb_title.innerHTML=thumb_title[i]
+  new_mainThumb.appendChild(new_mainThumb_title)
+
+
+  var new_mainThumb_userName=document.createElement("p");
+  new_mainThumb_userName.setAttribute("class","text_font")
+  new_mainThumb_userName.innerHTML=tumb_title_user[i]
+  new_mainThumb.appendChild(new_mainThumb_userName)
+
+
+  var new_mainTextSmall=document.createElement("div");
+  new_mainTextSmall.setAttribute("class","text_font")
+  new_mainThumb.appendChild(new_mainTextSmall)
+
+  var new_mainTextView=document.createElement("span");
+  new_mainTextView.innerHTML=tumb_view[i]
+  new_mainTextSmall.appendChild(new_mainTextView)
+
+  var new_mainTextDate=document.createElement("span");
+  new_mainTextDate.innerHTML="8개월 전"
+  new_mainTextSmall.appendChild(new_mainTextDate)
+
+
+}
 }
