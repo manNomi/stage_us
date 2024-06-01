b36c86dfc20f7b286957dbd745fcb1010bfe3d44
manNomi <hanmw110@naver.com>
2024-06-02 05:59:47 +0900

    main page ui done

--- 3주차 (24.05.29)/3주차 과제 파일/number_baseball.js
+++ 3주차 (24.05.29)/3주차 과제 파일/number_baseball.js
@@ -1,25 +1,26 @@
 introEvent()
 var first_check=true
 var computer_number_list=[]
+var main_game_top=document.getElementById("main_game_top")
+var main_game_bottom=document.getElementById("main_game_bottom")
 btnMake()
 defaultTextMake(3)
 backPageEvent()
 setDate()
 setInterval(setDate,1000);
+mainMenuMake()
 
+stageUsTitleEvent()
 function btnMake(){
     for(i=1;i<10;i++){
-        var main_bottom=document.getElementById("main_bottom")
+        var main_game_bottom=document.getElementById("main_game_bottom")
         var new_main_number_btn=document.createElement("button")
         new_main_number_btn.setAttribute("class","main_number_btn")
         new_main_number_btn.setAttribute("id",i)
-
-        main_bottom.appendChild(new_main_number_btn)
-
+        main_game_bottom.appendChild(new_main_number_btn)
         var new_main_number_text=document.createElement("P")
         new_main_number_text.innerHTML=i
         new_main_number_btn.appendChild(new_main_number_text)
-        
         btnClickEvnet(new_main_number_btn)
     }
 }
@@ -31,7 +32,7 @@ function defaultTextMake(number){
         main_present_text_.setAttribute("class","main_present_text")
         main_present_text_.setAttribute("id","main_present_text_"+i)
         main_present_text_.innerHTML="•"
-        main_present_text_shape.appendChild(main_present_text_)
+        document.getElementById("main_present_text_shape").appendChild(main_present_text_)
         number--
         i++
     }
@@ -73,10 +74,10 @@ function btnClickEvnet(btn){
 
 function vibration(){
     var result=document.getElementById("result")
-    result.setAttribute("style","animation: vibration 0.1s infinite;")
+    result.setAttribute("style","animation: vibration 0.2s infinite;")
     setTimeout(function() {
         result.setAttribute("style","animation: none;")
-    }, 400);
+    }, 800);
 }
 
 function vibrationMain(){
@@ -120,6 +121,7 @@ function randomNumber(){
         else if(i==2 && number!=0 && computer_number_list[0]!=number && computer_number_list[1]!=number){
             computer_number_list.push(number)
         }
+        
     }
     solve.innerHTML=computer_number_list
     return(computer_number_list)
@@ -157,25 +159,122 @@ function setDate(){
     var now = new Date()
     var hour=now.getHours()
     var min=now.getMinutes()
+    if(min<10){
+        min="0"+min
+    }
     header_present_time.innerHTML=hour+":"+min
 }
 
 function backPageEvent(){
     document.getElementById("baseball_btn").onclick=function(){
-    document.getElementById("main_top").setAttribute("style","display:none;")
-    document.getElementById("main_bottom").setAttribute("style","display:none;")
+        document.getElementById("main_page").setAttribute("style","display:block;")
+        downGamePage()
     }
 }
+
+function downGamePage(){
+    main_game_top.setAttribute("style","display:none;")
+    main_game_bottom.setAttribute("style","display:none;")
+}
+function openGamePage(){
+    main_game_top.setAttribute("style","display:flex;")
+    main_game_bottom.setAttribute("style","display:flex;")
+}
+
+function mainMenuBtnEvent(btns){
+    btns.onclick=function(){
+        if(btns.getAttribute("id")=="baseball"){
+            var main_page=document.getElementById("main_page")
+            main_page.setAttribute("style","display:none;")
+            openGamePage()
+        }
+        else{
+            alert("개발중입니다")
+        }
+    }
+}
+
 
 function introEvent(){
     var main=document.getElementById("main")
     main.setAttribute("style","animation: brightness 5s infinite;")
     setTimeout(
         function(){
-    main.setAttribute("style","animation: none;")
-
-        document.getElementById("main_bottom").setAttribute("style","display:flex;")
-        document.getElementById("main_top").setAttribute("style","display:flex;")
-        document.getElementById("intro").setAttribute("style","display:none;")
-        },5000)
+            main.setAttribute("style","animation: none;")
+            document.getElementById("intro").setAttribute("style","display:none;")
+            document.getElementById("main_page").setAttribute("style","display:block;")
+        },3000)
+    // 시간 설정
     }
+
+
+
+
+
+function mainMenuMake(){
+    id_list=["baseball","soccer","bascket"]
+    text_list=["숫자야구","미니축구","슬램덩크"]
+    img_list=["image/baseball_icon_menu_img.png","image/soccer_icon_menu_img.png","image/bascket_icon_menu_img.png"]
+    var main_page_menu=document.getElementById("main_page_menu")
+    for(i=0;i<text_list.length;i++){
+        var main_menu_item_box=document.createElement("div")
+        main_menu_item_box.setAttribute("class","main_menu_item_box")
+        main_page_menu.appendChild(main_menu_item_box)
+
+        var main_page_menu_left_box=document.createElement("div")
+        main_page_menu_left_box.setAttribute("class","main_page_menu_left_box")
+        main_menu_item_box.appendChild(main_page_menu_left_box)
+
+        var main_menu_thumb=document.createElement("img")
+        main_menu_thumb.setAttribute("class","main_menu_thumb")
+        main_menu_thumb.setAttribute("src",img_list[i])
+        main_page_menu_left_box.appendChild(main_menu_thumb)
+
+        var menu_text_box=document.createElement("div")
+        menu_text_box.setAttribute("class","menu_text_box")
+        main_page_menu_left_box.appendChild(menu_text_box)
+
+        var main_menu_text_top=document.createElement("p")
+        main_menu_text_top.innerHTML="-게임-"
+        main_menu_text_top.setAttribute("class","main_menu_text_top")
+        menu_text_box.appendChild(main_menu_text_top)
+
+        var main_menu_text_bottom=document.createElement("p")
+        main_menu_text_bottom.innerHTML=text_list[i]
+        main_menu_text_bottom.setAttribute("class","main_menu_text_bottom")
+        menu_text_box.appendChild(main_menu_text_bottom)
+
+        var space=document.createElement("span")
+        main_menu_item_box.appendChild(space)
+        
+        var main_menu_btns=document.createElement("button")
+        main_menu_btns.setAttribute("class","main_menu_btn")
+        main_menu_btns.setAttribute("id",id_list[i])
+        main_menu_item_box.appendChild(main_menu_btns)
+
+
+        var btn_text=document.createElement("p")
+        btn_text.innerHTML="시작"
+        main_menu_btns.appendChild(btn_text)
+        mainMenuBtnEvent(main_menu_btns)
+
+    }
+    var main_page_line=document.createElement("div")
+    main_page_line.setAttribute("id","main_page_line")
+    
+    main_page_menu.appendChild(main_page_line)
+
+
+    var score_check_btn=document.createElement("div")
+    score_check_btn.setAttribute("id","score_check_btn")
+    score_check_btn.innerHTML="내 점수•순위•포인트 보기 >"
+    main_page_menu.appendChild(score_check_btn)
+
+}
+
+function stageUsTitleEvent(){
+    var main_page_title=document.getElementById("main_page_title")
+    main_page_title.onclick=function(){
+        document.location.href="https://stageus.co.kr/faq";
+    }
+}
