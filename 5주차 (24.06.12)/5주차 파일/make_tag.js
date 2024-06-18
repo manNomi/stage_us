27073f467928880521ab02720114f637cd57e9ce
manNomi <hanmw110@naver.com>
2024-06-19 00:25:07 +0900

    post aside done

--- 5주차 (24.06.12)/5주차 파일/make_tag.js
+++ 5주차 (24.06.12)/5주차 파일/make_tag.js
@@ -42,20 +42,27 @@ function makeAsideIcon(){
     aside.appendChild(logo_text_box)
 }
 
+
 window.addEventListener("resize",function(){
     asideReSize()
 })
   window.onload=function(){
     var instar_icon = document.getElementById("instar_icon")
-    if (window.innerWidth>700){
+    if (window.innerWidth>1000){
         instar_icon.src="img/aside_long_icon/instar_text_logo.png"
         instar_icon.style.width="100px"
         type_check="long"
     }
-    else{
+    else if(window.innerWidth>850){
         instar_icon.src="img/aside_long_icon/instargram_logo.png"
         instar_icon.style.width="30px"
         type_check="short"
+    }
+    else{
+        document.getElementById("funciton_aside").style.display="none"
+        document.getElementById("funciton_footer").style.display="flex"
+        document.getElementById("posting_main").style.marginLeft=0
+        document.getElementById("story_header").style.marginLeft=0
     }
     asideReSize()
 }
@@ -82,18 +89,27 @@ function postReSize(size){
 
 
 function asideReSize(){
+    console.log(innerWidth)
     var small_size=80
     var big_size=160
-    if (window.innerWidth>700){
+    if (window.innerWidth>1000){
         postReSize(big_size)
         text_present="long"
         instarLogoAnimate(text_present)
 
     }
-    else{
+    else if(window.innerWidth>850){
+        document.getElementById("funciton_footer").style.display="none"
+        document.getElementById("funciton_aside").style.display="flex"
         postReSize(small_size)
         text_present="short"
         instarLogoAnimate(text_present)
+    }
+    else{
+        document.getElementById("funciton_aside").style.display="none"
+        document.getElementById("funciton_footer").style.display="flex"
+        document.getElementById("posting_main").style.marginLeft=0
+        document.getElementById("story_header").style.marginLeft=0
     }
 }
 
@@ -131,15 +147,159 @@ function instarLogoAnimate(type){
 
 // -----------------------------------------aside---------------------------------------------
 
-makePost()
-function makePost(){
-    const post_container=document.getElementById("posting_main")
+// makePost()
+function createPost(type) {
+    // Create main post container
+    const postBox = document.createElement('div');
+    postBox.className = 'post_box';
+    
+    // Create thumb and title container
+    const thumbAndTitle = document.createElement('div');
+    thumbAndTitle.className = 'thumb_and_title';
+    
+    
+    // Create and append post thumbnail image
+    const postThumb = document.createElement('img');
+    postThumb.className = 'post_thumb';
+    postThumb.src = 'img/post_icon/post_thumb/img1.PNG';
+    
+    // Create post title container
+    const postTitle = document.createElement('div');
+    postTitle.classList="post_title"
 
-    var tmp_post=document.createElement("div")
-    tmp_post.classList="post_box"
-    post_container.appendChild(tmp_post)
+    
+    // Create and append post title ID
+    const postTitleId = document.createElement('div');
+    postTitleId.className = 'post_title_id';
+    postTitleId.textContent = 'bop.pt';
+    postTitle.appendChild(postTitleId);
+    
+    // Create title user container
+    const titleUser = document.createElement('div');
+    titleUser.className = 'title_user';
+    
+    // Create and append post title sentence
+    const postTitleSentence = document.createElement('div');
+    postTitleSentence.className = 'post_title_sentence';
+    postTitleSentence.textContent = '300만이 선택한 요리...';
+    titleUser.appendChild(postTitleSentence);
+    
+    // Create and append title user "더 보기"
+    const postTitlePlus = document.createElement('div');
+    postTitlePlus.className = 'post_title_plus';
+    postTitlePlus.textContent = '더 보기';
+    titleUser.appendChild(postTitlePlus);
+    if(type=="right"){
+        thumbAndTitle.appendChild(postThumb);
+        thumbAndTitle.appendChild(postTitle);
+    }
+    else{
+        thumbAndTitle.appendChild(postTitle);
+        thumbAndTitle.appendChild(postThumb);
+    }
+    
+    postTitle.appendChild(titleUser);
+    
+    // Create post comment container
+    const postComment = document.createElement('div');
+    postComment.className = 'post_comment';
+    
+    // Create and append comment count
+    const commentCount = document.createElement('div');
+    commentCount.className = 'comment_count';
+    commentCount.textContent = '좋아요 997개';
+    postComment.appendChild(commentCount);
+    
+    // Function to create individual comment
+    function createComment(commentId, commentText) {
+        const commentSpecific = document.createElement('div');
+        commentSpecific.className = 'comment_specific';
+        
+        const commentIdDiv = document.createElement('div');
+        commentIdDiv.className = 'comment_id';
+        commentIdDiv.textContent = commentId;
+        commentSpecific.appendChild(commentIdDiv);
+        
+        const commentTextDiv = document.createElement('div');
+        commentTextDiv.className = 'comment_text';
+        commentTextDiv.textContent = commentText;
+        commentSpecific.appendChild(commentTextDiv);
+        
+        return commentSpecific;
+    }
+    
+    // Create and append comments
+    postComment.appendChild(createComment('bopbasd.ts', '왜 뭐를 했을까?'));
+    postComment.appendChild(createComment('bopbasd.ts', '왜 뭐를 했을까?'));
+    postComment.appendChild(createComment('bopbasd.ts', '왜 뭐를 했을까?'));
+    
+    // Create and append comment plus
+    const commentPlus = document.createElement('div');
+    commentPlus.className = 'comment_plus';
+    commentPlus.textContent = '댓글 달기...';
+    postComment.appendChild(commentPlus);
+    
+    postTitle.appendChild(postComment);
+    postBox.appendChild(thumbAndTitle);
+    
+    // Create title icon box
+    const titleIconBox = document.createElement('div');
+    titleIconBox.className = 'title_icon_box';
+    
+    function createTitleIconButton(iconSrc) {
+        const button = document.createElement('button');
+        button.className = 'title_icon_btn';
+        
+        const img = document.createElement('img');
+        img.className = 'title_icon';
+        img.src = iconSrc;
+        
+        button.appendChild(img);
+        return button;
+    }
+    
+    const saveIconButton = createTitleIconButton('img/post_icon/save_icon.png');
+    saveIconButton.className += ' save_icon';
 
-    var tmp_post=document.createElement("div")
-    tmp_post.classList="post_box"
-    post_container.appendChild(tmp_post)
+    if(type=="right"){
+        titleIconBox.appendChild(createTitleIconButton('img/post_icon/heart_icon.png'));
+        titleIconBox.appendChild(createTitleIconButton('img/post_icon/chat_icon.png'));
+        titleIconBox.appendChild(createTitleIconButton('img/post_icon/dm_icon.png'));
+        titleIconBox.appendChild(saveIconButton);
+        titleIconBox.style="flex-direction:row;"
+    }
+    else{
+        titleIconBox.appendChild(saveIconButton);
+        titleIconBox.appendChild(createTitleIconButton('img/post_icon/dm_icon.png'));
+        titleIconBox.appendChild(createTitleIconButton('img/post_icon/chat_icon.png'));
+        titleIconBox.appendChild(createTitleIconButton('img/post_icon/heart_icon.png'));
+        titleIconBox.style="flex-direction:row-reverse;"
+    }
+    postBox.appendChild(titleIconBox);
+    
+    const postingMain = document.getElementById('posting_main');
+    postingMain.appendChild(postBox);
+}
+for (i=0;i<2;i++){
+    createPost("right")
+    createPost("left")
+}
+// -----------------------------------------main---------------------------------------------
+makeFooterIcon()
+function makeFooterIcon(){
+    const footer_icon_list = ["home_icon","compass_icon","video_icon","plus_icon","dm_icon"]
+    function btnMake(img_src){
+        var button=document.createElement("button")
+        button.classList="footer_btn"
+        var img=document.createElement("img")
+        img.classList="footer_icon"
+        img.src=`img/footer_icon/${img_src}.png`
+        button.appendChild(img)
+        return button
+    }
+    const footer = document.getElementById("funciton_footer")
+    for(let i=0;i<footer_icon_list.length;i++){
+        footer.appendChild(btnMake(footer_icon_list[i]))
+        
+    }
 }
